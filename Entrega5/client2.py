ba225214c85f3c66d3275d729ccde49fd5533be7
giugal <glt_giuseppe@hotmail.it>
2015-03-27 15:51:59 +0100

    Practica 5
    Practica servidor-clientes

--- Entrega5/client2.py
+++ Entrega5/client2.py
@@ -6,8 +6,5 @@ HOST = '127.0.0.1'    # The remote host
 PORT = 8082          	 # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(sys.argv[1]+'/'+sys.argv[2]+'/'+sys.argv[3]+'/'+sys.argv[4])  #Habitacion
-#s.sendall(sys.argv[2]+'/')  #Sensor
-#s.sendall(sys.argv[3]+'/')  #Valor
-#s.sendall(sys.argv[4])  #Unidad de medida
+s.sendall(sys.argv[1]+'/'+sys.argv[2]+'/'+sys.argv[3]+'/'+sys.argv[4]) 
 s.close()
